bf2aabb4257d6be5ef7eeebd4f74856b9e661d9a
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-09-15 11:57:14 +0200

    used data from api to display wilders

--- wild-book-react/src/App.js
+++ wild-book-react/src/App.js
@@ -1,52 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 import Wilder from "./components/Wilder";
 
 function App() {
+  const dataManipulation = (dataFromApi) => {
+    const newData = dataFromApi.map((wilder) => {
+      const cleanSkills = wilder.grades.map((grade) => {
+        return { title: grade.skill.name, votes: grade.grade };
+      });
+      return { name: wilder.name, skills: cleanSkills };
+    });
+    return newData;
+  };
+  const [wilders, setWilders] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const wildersFromApi = await axios.get(
         "http://localhost:5000/api/wilders"
       );
-      console.log(wildersFromApi.data);
+      const formattedWilders = dataManipulation(wildersFromApi.data);
+      setWilders(formattedWilders);
     };
     fetchData();
   }, []);
-  const wildersData = [
-    {
-      name: "Ringo",
-      city: "London",
-      skills: [
-        { title: "JS", votes: 10 },
-        { title: "React", votes: 8 },
-      ],
-    },
-    {
-      name: "John",
-      city: "Paris",
-      skills: [
-        { title: "PHP", votes: 9 },
-        { title: "Symfony", votes: 9 },
-      ],
-    },
-    {
-      name: "George",
-      city: "Berlin",
-      skills: [
-        { title: "Ruby", votes: 10 },
-        { title: "JS", votes: 8 },
-      ],
-    },
-    {
-      name: "Paul",
-      city: "Reims",
-      skills: [
-        { title: "C++", votes: 10 },
-        { title: "Rust", votes: 8 },
-      ],
-    },
-  ];
   return (
     <div>
       <header>
@@ -57,7 +35,7 @@ function App() {
       <main className="container">
         <h2>Wilders</h2>
         <section className="card-row">
-          {wildersData.map((el, index) => (
+          {wilders.map((el, index) => (
             <Wilder
               key={index}
               name={el.name}
