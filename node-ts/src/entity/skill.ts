ae997f940775e44b07b49de4118ba428f38f8069
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-10-13 01:14:37 +0200

    Backend to typegraphql

--- node-ts/src/entity/skill.ts
+++ node-ts/src/entity/skill.ts
@@ -1,11 +1,15 @@
+import { Field, ObjectType } from "type-graphql";
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { Grade } from "./grade";
 
+@ObjectType()
 @Entity()
 export class Skill {
+  @Field()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   name: string;
 
